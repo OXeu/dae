8a111d5eb91de725d404d2bfd29c7339f3b49a92
xeufan <xeufan@tencent.com>
2025-05-18 10:15:29 +0800

    chore: change submodule upstream

--- control/connection_info.go
+++ control/connection_info.go
@@ -1,13 +1,13 @@
 package control
 
 type ConnectionInfo struct {
-    Src      string
-    Dst      string
-    Sniffed  string
-    Dscp     uint8
-    L4proto  string
-    Dialer   string
-    Outbound string
-    Mac      string
-    Pname    string
+	Src      string
+	Dst      string
+	Sniffed  string
+	Dscp     int
+	L4proto  string
+	Dialer   string
+	Outbound string
+	Mac      string
+	Pname    string
 }
